952c5182b764e29da3e9a7e7ab6643f3dec63ab9
chiller87 <tim-timmae@gmx.net>
2013-08-25 08:40:52 +0200

    added correct explanation

--- src/DepthFirstSearch.java
+++ src/DepthFirstSearch.java
@@ -20,6 +20,8 @@ public class DepthFirstSearch extends AbstractAlgorithm {
 	private HashMap<Vertex, Integer> dfb;
 	private HashMap<Vertex, Integer> dfe;
 	
+	private ArrayList<Edge> mD;
+	
 	@Override
 	public String getAlgorithmName() {
 		return "Tiefensuche";
@@ -37,7 +39,6 @@ public class DepthFirstSearch extends AbstractAlgorithm {
 	@Override
 	public void setRequirements(ArrayList<GraphElement> requirements) {
 		mStartVertex = (Vertex) requirements.get(0);
-		
 	}
 
 	@Override
@@ -50,20 +51,26 @@ public class DepthFirstSearch extends AbstractAlgorithm {
 		dfeCounter = 0;
 		dfb = new HashMap<Vertex, Integer>();
 		dfe = new HashMap<Vertex, Integer>();
+		mD = new ArrayList<Edge>();
 		System.out.println("starte tiefensuche mit Vertex "+ mStartVertex);
 		depthFirstSearch(mStartVertex);
+		addStep("red = Tree Edge\ngreen = Back Edge\nblue = Cross Edge\nyellow = Forward Edge");
 	}
 	
 	
 	private void depthFirstSearch(Vertex u) {
-		u.setState(ElementState.VISITED);
+		u.setState(ElementState.ACTIVE);
 		dfb.put(u, dfbCounter);
 		System.out.println("Vertex "+ u +" visited");
 		dfbCounter++;
 		u.setLabelAddition("("+ dfb.get(u) +", %)");
+		
 		Collection<Edge> edges = mGraph.getOutEdges(u);
-		addStep("D = "+ edges);
+		addNewEdges(edges);
+		
+		addStep("D = "+ mD);
 		for(Edge e : edges) {
+			mD.remove(e);
 			System.out.println("Edge "+ e);
 			Vertex v = e.getOtherEnd(u);
 			System.out.println("other end of Edge "+ e +" is Vertex "+ v);
@@ -71,15 +78,21 @@ public class DepthFirstSearch extends AbstractAlgorithm {
 				e.setCustomColor(Color.decode("#FF0000")); // rot, Baumkante
 				depthFirstSearch(v);
 			}
-			else if(v.getState() == ElementState.VISITED) {
+			else if((v.getState() == ElementState.ACTIVE) || (v.getState() == ElementState.VISITED)) {
 				System.out.println("Vertex "+ v +" is already visited");
 				System.out.println("dfb(v)="+ dfb.get(v) +", dfb(u)="+ dfb.get(u) +", dfe(v)="+ dfe.get(v) +", dfe(u)="+ dfe.get(u));
-				if((dfb.get(v) < dfb.get(u)) && (dfe.get(v) != null))
+				if((dfb.get(v) < dfb.get(u)) && (dfe.get(v) != null)) {
 					e.setCustomColor(Color.decode("#0000FF")); // blau, Querkante
-				else if((dfb.get(v) < dfb.get(u)) && (dfe.get(v) == null))
+					addStep("Edge "+ e +" marked as Cross Edge (blue) because\ndfb(v)="+ dfb.get(v) +" < dfb(u)="+ dfb.get(u) +" and dfe(v)="+ dfe.get(v) +" < dfe(u)=%\nD = "+ mD);
+				}
+				else if((dfb.get(v) < dfb.get(u)) && (dfe.get(v) == null)) {
 					e.setCustomColor(Color.decode("#3FD121")); // green, Rückwärtskante
-				else if(dfb.get(v) > dfb.get(u))
+					addStep("Edge "+ e +" marked as Back Edge (green) because\ndfb(v)="+ dfb.get(v) +" < dfb(u)="+ dfb.get(u) +" and dfe(v)=% > dfe(u)=%\nD = "+ mD);
+				}
+				else if(dfb.get(v) > dfb.get(u)) {
 					e.setCustomColor(Color.decode("#F2E30F")); // gelb, Vorwärtskante
+					addStep("Edge "+ e +" marked as Forward Edge (yellow) because\ndfb(v)="+ dfb.get(v) +" > dfb(u)="+ dfb.get(u) +"\nD = "+ mD);
+				}
 				
 				System.out.println("custom color of Edge "+ e +": "+ e.getCustomColor());
 			}
@@ -87,9 +100,17 @@ public class DepthFirstSearch extends AbstractAlgorithm {
 		dfe.put(u, dfeCounter);
 		dfeCounter++;
 		u.setLabelAddition("("+ dfb.get(u) +", "+ dfe.get(u) +")");
+		u.setState(ElementState.VISITED);
 		addStep("finished Vertex "+ u);
 	}
+
 	
-	
+	private void addNewEdges(Collection<Edge> c) {
+		ArrayList<Edge> newD = new ArrayList<Edge>(c);
+		for(Edge e : mD)
+			newD.add(e);
+		
+		mD = newD;
+	}
 
 }
